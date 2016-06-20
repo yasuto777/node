86dcfc2684458199e427161035fc2156747bd571
yasuto777 <basilisk.0331@icloud.com>
2016-06-20 14:54:39 +0000

    ejs example

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
 var server = http.createServer();
 var settings = require('./settings');
-
-var msg;
+var templete = fs.readFileSync(__dirname + '/public_html/hello.ejs','utf-8');
+var n = 0;
 
 server.on('request',function(req,res){
-	fs.readFile(__dirname + '/public_html/hello.html','utf-8',function(err,data){
-		if (err){
-	res.writeHead(404, {'content-Type':'text/plain'});
-	res.write("not found");
-	return res.end();
-		}
+	n++;
+	var data = ejs.render(templete,{
+		title : "hello",
+			content : "<strong>World!</strong>",
+			n : n
+	});
 	res.writeHead(200, {'content-Type':'text/html'});
 	res.write(data);
 	res.end();
-	});
 });
 
 //server.listen(settings.port);
