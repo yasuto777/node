cfbc26bd6f62ad57dcae770ece6e3d57ff38d7fb
yasuto777 <basilisk.0331@icloud.com>
2016-06-23 00:15:18 +0900

    Error location is not know where it is

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 var ejs = require('ejs');
+var qs = require('querystring');
+
 var server = http.createServer();
 var settings = require('./settings');
-var templete = fs.readFileSync(__dirname + '/public_html/hello.ejs','utf-8');
-var n = 0;
+var templete = fs.readFileSync(__dirname + '/public_html/bbs.ejs','utf-8');
+var posts = [];
 
-server.on('request',function(req,res){
-	n++;
+function renderForm(posts,res){
 	var data = ejs.render(templete,{
-		title : "hello",
-			content : "<strong>World!</strong>",
-			n : n
+		posts: posts
 	});
 	res.writeHead(200, {'content-Type':'text/html'});
 	res.write(data);
 	res.end();
+}
+
+server.on('request',function(req,res){
+	if(req.method === 'POST'){
+	req.data = "";
+	req.on("readable",function(){
+		req.data += req.read();
+	});
+	req.on("end",function(){
+	var query = qs.parse(req.data);
+	posts.push(query.name);
+	renderForm(posts,res);
+	});
+	}else{
+	renderForm(posts,res);
+	}
+
 });
 
 //server.listen(settings.port);
