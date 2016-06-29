e68bacde944708a7d8156b1539f2417af44030f9
yasuto777 <basilisk.0331@icloud.com>
2016-06-29 23:18:55 +0900

    Split script

--- editor/editor.js
+++ editor/editor.js
@@ -1,3 +1,7 @@
-var ace = require('./lib/ace');
-
-ace.edit("editor");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
+//折り返し有効化
+editor.getSession().setUseWrapMode(true);
+//タブ幅2に設定
+editor.getSession().setTabSize(2);
