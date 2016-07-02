0db960aa53c2daaa36523fe283228db565e679a0
yasuto777 <basilisk.0331@icloud.com>
2016-07-02 23:44:46 +0900

    Use bootstrap

--- editor/editor.js
+++ editor/editor.js
@@ -1,6 +1,18 @@
 var editor = ace.edit("editor");
+editor.$blockScrolling = Infinity;
+editor.setOptions({
+    //自動補完
+    enableBasicAutocompletion: true
+    , //スニペット
+    enableSnippets: true
+    , //ライブ補完
+    enableLiveAutocompletion: true
+});
+
+//テーマの設定
 editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
+//モードの設定(Markdown)
+editor.getSession().setMode("ace/mode/markdown");
 //折り返し有効化
 editor.getSession().setUseWrapMode(true);
 //タブ幅2に設定
